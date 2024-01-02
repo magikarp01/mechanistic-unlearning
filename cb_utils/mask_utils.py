f3a955ef26757886662cadbe54b9a47733a882c1
magikarp01 <philliphguo@gmail.com>
2024-01-02 19:10:22 +0000

    mask utils done

--- cb_utils/mask_utils.py
+++ cb_utils/mask_utils.py
@@ -43,7 +43,7 @@ def get_default_gpt2_param_names(num_layers=12):
         default_param_names.append(f'blocks.{i}.edge_mask_mlp')
     return default_param_names
 
-def get_default_edge_mask_dict(num_layers=12, num_heads=12):
+# def get_default_edge_mask_dict(num_layers=12, num_heads=12):
 
 
 
@@ -59,6 +59,49 @@ def reset_mask(model):
     for name, param in model.named_parameters():
         if param.requires_grad:
             param.data = torch.ones_like(param.data).to(param.device)
+
+
+def get_all_possible_nodes(mask_params=None, param_names=None, num_layers=12, num_heads=12, ):
+    """
+    Get node names in format of 
+    [(-1, 'embed'),
+    (0, 'a0.0'),
+    (0, 'a0.1'),
+    (0, 'a0.2'),
+    (0, 'a0.3'),
+    (0, 'a0.4'),
+    (0, 'a0.5'),
+    (0, 'm0'),
+    ...,
+    (12, 'output')]
+    If mask_params and param_names are provided, then will use that + num_heads. If not, will use default param names for num_layers and num_heads.
+    """
+    if mask_params is not None and param_names is not None:
+        # associate each node with a position
+        all_possible_nodes = [(-1, "embed")]
+        mask_dict = {}
+        # empty tensor
+        mask_dict["embed"] = torch.zeros(size=(0,))
+        for idx in range(len(mask_params)):
+            if "attention" in param_names[idx]:
+                layer = int(param_names[idx].split(".")[1])
+                for i in range(num_heads):
+                    all_possible_nodes.append((layer, f"a{layer}.{i}"))
+                    mask_dict[f"a{layer}.{i}"] = mask_params[idx][:,i].detach().cpu()
+            elif "mlp" in param_names[idx]:
+                layer = int(param_names[idx].split(".")[1])
+                all_possible_nodes.append((layer, f"m{layer}"))
+                mask_dict[f"m{layer}"] = mask_params[idx].detach().cpu()
+        all_possible_nodes.append((num_layers, "output"))
+    else:
+        all_possible_nodes = [(-1, "embed")]
+        for layer in range(num_layers):
+            for head in range(num_heads):
+                all_possible_nodes.append((layer, f"a{layer}.{head}"))
+            all_possible_nodes.append((layer, f"m{layer}"))
+        all_possible_nodes.append((num_layers, "output"))
+    return all_possible_nodes
+
 
 def get_nodes_and_edges(mask_params, param_names=None, edge_0=True):
     """
@@ -111,7 +154,7 @@ def get_nodes_and_edges(mask_params, param_names=None, edge_0=True):
 # acdcpp edges are in format 'blocks.1.attn.hook_result[:, :, 10]blocks.0.hook_mlp_in[:]', convert to format of ((1, 'a1.10'), (0, 'm0'))
 
 def get_node_name(node_name, show_full_index=False):
-    """Node name for use in pretty graphs"""
+    """Turn acdcpp node name into the circuit breaking node name. Flattens q, k, v of attention heads all into just attn."""
 
     def get_index(node_name_long):
         # Get the index by looking for number in brackets
@@ -179,3 +222,92 @@ def get_node_name(node_name, show_full_index=False):
     
 
     return layer, name
+
+def get_edge_mask_template(num_layers=12, num_heads=12):
+    edge_mask_template = {}
+    edge_mask_template["embed"] = torch.zeros(size=(0,))
+    num_prev_components = 1
+    for layer in range(num_layers):
+        for head in range(num_heads):
+            edge_mask_template[f"a{layer}.{head}"] = torch.zeros(size=(num_prev_components,))
+        num_prev_components += num_heads
+        edge_mask_template[f"m{layer}"] = torch.zeros(size=(num_prev_components,))
+        num_prev_components += 1
+    edge_mask_template["output"] = torch.zeros(size=(num_prev_components,))
+    return edge_mask_template
+
+# Convert edges back to edge mask
+def get_mask_from_edges(edges, edge_mask_template=None, all_possible_nodes=None, edge_0=True, num_layers=12, num_heads=12):
+    """
+    edges is a set of e.g. {((0, 'a0.1'), (-1, 'embed')),
+        ((0, 'a0.10'), (-1, 'embed')),
+        ((2, 'a2.2'), (-1, 'embed')),
+        ((2, 'a2.2'), (0, 'a0.1')),}
+        
+    edge_mask_template: Doesn't matter what values, just needs shape of 
+        {'embed': tensor([]),
+        'a0.0': tensor([0.]),
+        'a0.1': tensor([1.]),
+        'a0.2': tensor([0.]),
+        'a0.3': tensor([1.]),
+        'a0.4': tensor([0.]),
+        'a0.5': tensor([1.]),
+        'm0': tensor([1., 0., 1., 0., 0., 1., 0., 0., 0., 0., 0., 1., 0.]),
+        ...}
+
+    all_possible_nodes needs shape of
+        [(-1, 'embed'),
+        (0, 'a0.0'),
+        (0, 'a0.1'),
+        (0, 'a0.2'),
+        (0, 'a0.3'),
+        (0, 'a0.4'),
+        (0, 'a0.5'),
+        (0, 'm0'),
+        ...,
+        (12, 'output')]
+
+    Outputs an edge_mask with actual edges with same form of weight_mask_template
+    """
+    if edge_mask_template is None:
+        edge_mask_template = get_edge_mask_template(num_layers=num_layers, num_heads=num_heads)
+
+    if all_possible_nodes is None:
+        all_possible_nodes = get_all_possible_nodes(num_layers=num_layers, num_heads=num_heads)
+    
+    new_mask_dict = {}
+    for node_name in edge_mask_template:
+        new_mask_dict[node_name] = torch.ones_like(edge_mask_template[node_name]) if edge_0 else torch.zeros_like(edge_mask_template[node_name])
+    
+    node_indices = {node_name: idx for idx, node_name in enumerate(all_possible_nodes)}
+    for edge in edges:
+        try:
+            new_mask_dict[edge[0][1]][node_indices[edge[1]]] = 0 if edge_0 else 1
+        except:
+            continue
+    return new_mask_dict
+
+def convert_mask_dict_to_params(mask_dict):
+    """
+    Takes a mask dict in format of 
+        {'embed': tensor([]),
+        'a0.0': tensor([0.]),
+        'a0.1': tensor([1.]),
+        'a0.2': tensor([0.]),
+        'a0.3': tensor([1.]),
+        'a0.4': tensor([0.]),
+        'a0.5': tensor([1.]),
+        'm0': tensor([1., 0., 1., 0., 0., 1., 0., 0., 0., 0., 0., 1., 0.]),
+        ...}
+    Returns mask params (that can be used in model state dict, look like mask_params, can be used in get_nodes_and_edges()).
+    """
+    mask_params = []
+    # first output_mask
+    mask_params.append(mask_dict["output"])
+    for layer in range(12):
+        attn_tensors = []
+        for head in range(12):
+            attn_tensors.append(mask_dict[f"a{layer}.{head}"])
+        mask_params.append(torch.stack(attn_tensors, dim=1))
+        mask_params.append(mask_dict[f"m{layer}"])
+    return mask_params
