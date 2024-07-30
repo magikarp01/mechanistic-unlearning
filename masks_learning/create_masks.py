652b5d1eecd77ef76e8590cda4644f16096bc5b5
Aaquib111 <aaquibahm@gmail.com>
2024-07-30 07:38:19 +0000

    added gemma 7b ioi localization code

--- masks_learning/create_masks.py
+++ masks_learning/create_masks.py
@@ -13,8 +13,8 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 #%%
 ### LOAD MODELS
-model_name = 'EleutherAI/pythia-2.8b'
-    #  'google/gemma-7b' 
+model_name = 'google/gemma-7b' 
+#'EleutherAI/pythia-2.8b'
     # 'meta-llama/Meta-Llama-3-8B'
     # 'Qwen/Qwen1.5-4B' 
 
@@ -45,19 +45,50 @@ from tasks.facts.SportsTask import SportsFactsTask
 
 # ind_task = InductionTask(batch_size=25, tokenizer=tokenizer, prep_acdcpp=True, device=device)
 # ind_task.set_logit_diffs(model)
-# ioi_task = IOITask(batch_size=25, tokenizer=tokenizer, device=device, prep_acdcpp=True)
-# ioi_task.set_logit_diffs(model)
-sports_task = SportsFactsTask(
-    model=model, 
-    batch_size=5, 
-    tokenizer=tokenizer,
-    N=25, 
-    # forget_sport_subset={"football"},
-    # forget_player_subset={"Austin Rivers"},
-    # is_forget_dataset=True,
-    device=device
-)
+ioi_task = IOITask(batch_size=25, tokenizer=tokenizer, device=device, prep_acdcpp=True)
+ioi_task.set_logit_diffs(model)
+# sports_task = SportsFactsTask(
+#     model=model, 
+#     batch_size=5, 
+#     tokenizer=tokenizer,
+#     N=25, 
+#     # forget_sport_subset={"football"},
+#     # forget_player_subset={"Austin Rivers"},
+#     # is_forget_dataset=True,
+#     device=device
+# )
 
+save_model_name = model_name.replace('/', '_')
+for name, task in zip(["ioi"], [ioi_task]):
+    eap_localizer = EAPLocalizer(model, task)
+    ap_localizer = APLocalizer(model, task)
+    ct_localizer = CausalTracingLocalizer(model, task)
+
+
+    ### GET ATTRIBUTION SCORES FROM LOCALIZATIONS
+    torch.cuda.empty_cache()
+    gc.collect()
+    eap_graph = eap_localizer.get_exp_graph(batch=1, threshold=-1)
+    top_edges = eap_graph.top_edges(n=len(eap_graph.eap_scores.flatten()), threshold=-1)
+    with open(f"models/{save_model_name}_{name}_eap_graph.pkl", "wb") as f:
+        pickle.dump(top_edges, f)
+
+    torch.cuda.empty_cache()
+    gc.collect()
+    ap_graph = ap_localizer.get_ap_graph(batch_size=1)
+    torch.cuda.empty_cache()
+    gc.collect()
+    with open(f"models/{save_model_name}_{name}_ap_graph.pkl", "wb") as f:
+        pickle.dump(dict(ap_graph), f)
+
+    model.eval() # Don't need gradients when doing ct task
+    ct_graph = ct_localizer.get_ct_mask(batch_size=2)
+    model.train()
+    with open(f"models/{save_model_name}_{name}_ct_graph.pkl", "wb") as f:
+        pickle.dump(dict(ct_graph), f)
+
+    torch.cuda.empty_cache()
+    gc.collect()
 #%%
 ### LOAD LOCALIZATION METHODS
 from localizations.eap.localizer import EAPLocalizer
