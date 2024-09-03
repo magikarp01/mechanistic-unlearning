c6565bc1290ea36c8fccaab4a90a37561cb2ec1c
magikarp01 <philliphguo@gmail.com>
2024-09-03 08:19:14 +0000

    addition task interp

--- notebook_slurm.sh
+++ notebook_slurm.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=jupyter
 #SBATCH --output=jupyter_logs/log-%J.txt
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=42
+# #SBATCH --cpus-per-task=42
 #SBATCH --tasks-per-node=1
 #SBATCH --gres=gpu:1
-#SBATCH --time=3:00:00
+#SBATCH --time=4:00:00
 
 source /data/phillip_guo/miniconda3/etc/profile.d/conda.sh
 conda activate unlrn
