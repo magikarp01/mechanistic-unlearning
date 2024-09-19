fde9ebbe1e4e2a35e975d28527ab6bb77c7f8e07
Aaquib111 <aaquibahm@gmail.com>
2024-09-19 21:59:42 +0000

    CT localization, p efficient

--- localizations/causal_tracing/localizer.py
+++ localizations/causal_tracing/localizer.py
@@ -32,7 +32,7 @@ class RunType(enum.Enum):
     PATCH = 2
 
 class CausalTracingLocalizer():
-    def __init__(self, model, toks, noise_indices, correct_toks, incorrect_toks=None):
+    def __init__(self, model, toks, noise_indices, correct_toks, incorrect_toks=None, verbose=False):
         ''' 
         Creates a causal tracing localizer object
 
@@ -49,19 +49,18 @@ class CausalTracingLocalizer():
         self.incorrect_toks = incorrect_toks
 
         # ct params: noise level, clean cache
-        self.clean_cache = model.run_with_cache(
+        _, self.save_cache = model.run_with_cache(
             self.toks,
-
         )
         self.noise_level = torch.sqrt(3 * self.get_embedding_std())
 
         # Model has (n_layers, n_heads+1) components (including MLP)
         # Probability of outputting correct result, clean model
-        self.p_clean = self.patch_model_components(run_type=RunType.CLEAN)
+        self.p_clean = self.do_clean_run()
         # Probability of outputting correct result, corrupt model
-        self.p_corr = self.patch_model_components(run_type=RunType.CORRUPT)
+        self.p_corr = self.do_corrupt_run()
         # Probability of outputting correct result, patched model
-        self.p_patch = self.patch_model_components(run_type=RunType.PATCH)
+        self.p_patch = self.patch_model_components(verbose=verbose)
 
 
     def get_total_effect(self):
@@ -72,12 +71,15 @@ class CausalTracingLocalizer():
 
     def get_embedding_std(self):
         # Get embedding std of import tokens, i.e the tokens we will be noising
-        noise_tokens = self.toks[self.noise_indices]
-        return model.W_E[noise_tokens].std()
+        
+        noise_stds = []
+        for idx in self.noise_indices:
+            noise_stds.append(model.W_E[self.toks[idx]].std().item())
+        return torch.tensor(noise_stds)
     
     def get_correct_prob(self, logits):
         # Return sum probability of correct token(s) at last token position, avg over batch
-        return torch.nn.functional.softmax(logits[:, -1, self.correct_toks], dim=-1).mean(0).sum()
+        return torch.nn.functional.softmax(logits[:, -1, :], dim=-1)[:, self.correct_toks].mean(0).sum()
     
     def ct_embedding_noise_hook(self, act, hook):
         '''
@@ -89,7 +91,7 @@ class CausalTracingLocalizer():
                 # Noise inds of shape (batch, num_noise_inds)
                 # Iterate through batch and noise num_noise_inds
                 for i in range(act.shape[0]):
-                    act[i, self.noise_indices[i], :] = act[i, self.noise_indices[i], :] + torch.randn_like(act[i, self.noise_indices[i], :]) * self.noise_level
+                    act[i, self.noise_indices[i], :] = act[i, self.noise_indices[i], :] + torch.randn_like(act[i, self.noise_indices[i], :]) * self.noise_level[i]
             else:
                 act[:, self.noise_indices, :] = act[:, self.noise_indices, :] + torch.randn_like(act[:, self.noise_indices, :]) * self.noise_level
         return act
@@ -108,7 +110,30 @@ class CausalTracingLocalizer():
                 act = self.save_cache[hook.name]
         return act
 
-    def patch_model_components(self, run_type=RunType.PATCH, verbose=False):
+    def do_clean_run(self):
+        results_mat = torch.ones((model.cfg.n_layers, model.cfg.n_heads+1))
+        model.reset_hooks()
+        patched_logits = model(self.toks)
+        model.reset_hooks()
+        results_mat *= self.get_correct_prob(patched_logits).item()
+
+        return results_mat 
+
+    def do_corrupt_run(self):
+        results_mat = torch.ones((model.cfg.n_layers, model.cfg.n_heads+1))
+        model.reset_hooks()
+        patched_logits = model.run_with_hooks(
+            self.toks,
+            fwd_hooks = [
+                (utils.get_act_name('embed'), self.ct_embedding_noise_hook)
+            ]
+        )
+        model.reset_hooks()
+        results_mat *= self.get_correct_prob(patched_logits).item()
+
+        return results_mat 
+
+    def patch_model_components(self, verbose=False):
         results_mat = torch.zeros((model.cfg.n_layers, model.cfg.n_heads+1))
         for layer in tqdm(list(range(model.cfg.n_layers))):
             for head in tqdm(list(range(model.cfg.n_heads))):
@@ -120,30 +145,19 @@ class CausalTracingLocalizer():
                 )
                 model.reset_hooks()
 
-                if run_type == RunType.CLEAN:
-                    if verbose:
-                        print(f'Running clean on {layer}, {head}')
-                    fwd_hooks = []
-                elif run_type == RunType.CORRUPT:
-                    if verbose:
-                        print(f'Running corrupt on {layer}, {head}')
-                    fwd_hooks = [
-                        (utils.get_act_name('embed'), self.ct_embedding_noise_hook)
-                    ]
-                elif run_type == RunType.PATCH:
-                    if verbose:
-                        print(f'Running patch on {layer}, {head}')
-                    fwd_hooks = [
-                        (utils.get_act_name('embed'), self.ct_embedding_noise_hook),
-                        (utils.get_act_name('z', layer), patch_hook)
-                    ]
+                fwd_hooks = [
+                    (utils.get_act_name('embed'), self.ct_embedding_noise_hook),
+                    (utils.get_act_name('z', layer), patch_hook)
+                ]
 
                 patched_logits = model.run_with_hooks(
                     self.toks,
                     fwd_hooks=fwd_hooks
                 )
                 model.reset_hooks()
-                results_mat[layer, head] = self.get_correct_prob(patched_logits)
+                results_mat[layer, head] = self.get_correct_prob(patched_logits).item()
+                if verbose:
+                    print('Layer', layer, 'Head', head, 'Correct prob', results_mat[layer, head])
             # Do MLP
             patch_hook = functools.partial(
                 self.ct_hook,
@@ -151,41 +165,74 @@ class CausalTracingLocalizer():
                 save_head=None
             )
             model.reset_hooks()
-            if run_type == RunType.CLEAN:
-                if verbose:
-                    print(f'Running clean on {layer}, mlp')
-                fwd_hooks = []
-            elif run_type == RunType.CORRUPT:
-                if verbose:
-                    print(f'Running corrupt on {layer}, mlp')
-                fwd_hooks = [
-                    (utils.get_act_name('embed'), self.ct_embedding_noise_hook)
-                ]
-            elif run_type == RunType.PATCH:
-                if verbose:
-                    print(f'Running patch on {layer}, mlp')
-                fwd_hooks = [
-                    (utils.get_act_name('embed'), self.ct_embedding_noise_hook),
-                    (utils.get_act_name('post', layer), patch_hook)
-                ]
+
+            fwd_hooks = [
+                (utils.get_act_name('embed'), self.ct_embedding_noise_hook),
+                (utils.get_act_name('post', layer), patch_hook)
+            ]
             patched_logits = model.run_with_hooks(
                 self.toks,
                 fwd_hooks=fwd_hooks
             )
             model.reset_hooks()
-            results_mat[layer, model.cfg.n_heads] = self.get_correct_prob(patched_logits)
+            results_mat[layer, model.cfg.n_heads] = self.get_correct_prob(patched_logits).item()
+            if verbose:
+                print('Layer', layer, 'MLP Correct prob', results_mat[layer, model.cfg.n_heads])
 
-        if run_type == RunType.CLEAN:
-            self.p_clean = results_mat
-        elif run_type == RunType.CORRUPT:
-            self.p_corr = results_mat
-        elif run_type == RunType.PATCH:
-            self.p_patch = results_mat
+        return results_mat
 
 #%%
-localizer = CausalTracingLocalizer(model, )
-prompt_toks = model.tokenizer.
+import numpy as np
+
+def find_sublist_indices(tensor, sublists):
+    # Store indices of starting positions for each sublist
+    indices = []
+    
+    # Iterate over each row in the tensor and corresponding sublist
+    for row, sublist in zip(tensor, sublists):
+        row_len = len(row)
+        sublist_len = len(sublist)
+        found_index = -1  # Default value if sublist is not found
+        
+        # Slide over the row to find the sublist
+        for i in range(row_len - sublist_len + 1):
+            if np.array_equal(row[i:i+sublist_len], sublist):
+                found_index = list(range(i, i+sublist_len))
+                break
+        
+        indices.append(found_index)
+    
+    return indices
+
+#%%
+
 # 'prompt' is list of string prompts
 # 'subject' is the string main subject of the prompt
 # 'first_token' is int correct answer first tokens
-model.tokenizer()
+prompt_toks = model.tokenizer(
+    forget_fact_eval.train_df['prompt'].tolist(),
+    padding=True,
+    return_tensors="pt"
+)['input_ids']
+
+subject_toks = model.tokenizer(
+    forget_fact_eval.train_df['subject'].tolist(),
+    add_special_tokens=False
+)['input_ids']
+
+subject_idxs = find_sublist_indices(prompt_toks, subject_toks)
+
+correct_toks = torch.tensor(
+    forget_fact_eval.train_df['first_token'].tolist()
+)
+
+#%%
+localizer = CausalTracingLocalizer(
+    model,
+    toks=prompt_toks,
+    noise_indices=subject_idxs,
+    correct_toks=correct_toks,
+    incorrect_toks=None,
+    verbose=False
+)
+# %%
