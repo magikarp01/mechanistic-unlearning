a36be56965a2168db31c78f03f14844261ea906d
Aaquib111 <aaquibahm@gmail.com>
2025-03-31 19:36:01 +0000

    rebuttal scaling experiments done

--- experiments/scaling_counterfact/eval_scaling_autocast.py
+++ experiments/scaling_counterfact/eval_scaling_autocast.py
@@ -14,8 +14,8 @@ import random
 #%%
 
 MODEL_NAME = "google/gemma-7b"
-DATASET_LENGTH = 30
-MODE = "MANUAL"
+DATASET_LENGTH = 300
+MODE = "ALL"
 DTYPE = torch.bfloat16
 
 device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
@@ -33,8 +33,9 @@ mcq_path = "/root/mechanistic-unlearning/tasks/facts/data/counterfact_mc_questio
 cfact_path = "/root/mechanistic-unlearning/experiments/scaling_counterfact/cfact_forget_google_gemma-7b.csv"
 mcq = pd.read_parquet(mcq_path)
 mcq['case_id'] = mcq['prompt_id']
-cfact = pd.read_csv(cfact_path)
+cfact = pd.read_csv(cfact_path)[:DATASET_LENGTH]
 merged_q = pd.merge(mcq, cfact, how="inner", on="case_id")
+print(f"{len(merged_q)=}")
 # %%
 def collate_fn(batch, tokenizer):
     def generate_mcqs(questions, correct_answers, incorrect_answers, edit_answers):
@@ -44,12 +45,20 @@ def collate_fn(batch, tokenizer):
         icl_prompt = "Where is the Eiffel Tower?\nA. Seattle\nB. Rome\nC. Paris\nD. Madrid\nAnswer: C\n"
 
         for q, correct, incorrect, edit in zip(questions, correct_answers, incorrect_answers, edit_answers):
-            # Combine correct and incorrect answers
             options = list(incorrect) + [correct]
             random.shuffle(options)
 
             # Map options to labels
             option_labels = ['A', 'B', 'C', 'D']
+
+            # Find correct answer's label
+            correct_idx = options.index(correct)
+            correct_label = option_labels[correct_idx]
+            if edit not in options:
+                options[(correct_idx+1)%4] = edit
+
+            edit_label = option_labels[options.index(edit)]
+
             labeled_options = [f"{label}. {opt}" for label, opt in zip(option_labels, options)]
 
             # Format question string
@@ -57,9 +66,6 @@ def collate_fn(batch, tokenizer):
             # question_text = f"{q}\n" + "\n".join(labeled_options) + "\nAnswer:"
             formatted_questions.append(question_text)
 
-            # Find correct answer's label
-            correct_label = option_labels[options.index(correct)]
-            edit_label = option_labels[options.index(edit)]
             orig_labels.append(" " + correct_label)
             edit_labels.append(" " + edit_label)
 
@@ -109,14 +115,52 @@ from functools import partial
 dl = DataLoader(
     Dataset.from_pandas(merged_q), 
     collate_fn=partial(collate_fn, tokenizer=model.tokenizer), 
-    batch_size=4
+    batch_size=1100
 )
+# Logistic regression
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import numpy as np
 
+
+classes = {' A': 0, ' B': 1, ' C': 2, ' D': 3}
 with torch.no_grad():
     for (question_toks, orig_labels, edit_labels) in dl:
         question_toks = question_toks.to(device)
         orig_labels = orig_labels.to(device)
         edit_labels = edit_labels.to(device)
 
-        with torch.autocast(device_type=device, dtype=DTYPE):
-            model.run_with_cache()
+        orig_strs = model.tokenizer.batch_decode(orig_labels.reshape(-1, 1))
+        orig_classes = np.array([classes[s] for s in orig_strs])
+
+        with torch.cuda.amp.autocast(dtype=DTYPE):
+            _, cache = model.run_with_cache(
+                question_toks,
+                names_filter=lambda name: "resid_pre" in name
+            )
+            X = np.stack([v[:, -1, :].detach().cpu().numpy() for v in cache.values()], axis=1) # batch n_layers n_feat
+            y = orig_classes
+
+            # Train per layer
+            layer_accs = []
+            for layer in range(X.shape[1]):
+                X_layer = X[:, layer, :]
+                X_train, X_test, y_train, y_test = train_test_split(X_layer, y, test_size=0.5)
+                clf = LogisticRegression(max_iter=1000, solver="sag").fit(X_train, y_train)
+                print(f"Layer {layer}: {clf.score(X_test, y_test)}")
+                layer_accs.append(clf.score(X_test, y_test))
+        break
+
+
+# %%
+# Dump the results in a json file
+import json
+with open(f"/root/mechanistic-unlearning/experiments/scaling_counterfact/{MODEL_NAME.replace('/', '_')}_forget_{DATASET_LENGTH}_{MODE}_layer_accs.json", "w") as f:
+    data = {
+        "model": MODEL_NAME,
+        "dataset_length": DATASET_LENGTH,
+        "mode": MODE,
+        "layer_accs": layer_accs
+    }
+    json.dump(data, f)
+# %%
---
+++ experiments/scaling_counterfact/finetune_scaling_autocast.py
@@ -0,0 +1,369 @@
+#%%
+import torch
+from tqdm import tqdm
+from transformer_lens import HookedTransformer
+from transformers import DataCollatorForLanguageModeling
+import datasets
+from datasets import load_dataset, Dataset
+from torch.utils.data import DataLoader
+import pandas as pd
+import numpy as np
+import random
+import gc
+
+
+#%%
+DATASET_LENGTH = 300
+MODE = "ALL"
+MODEL_NAME = "google/gemma-7b"
+DTYPE = torch.bfloat16
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = HookedTransformer.from_pretrained(MODEL_NAME, default_padding_side="left", device=device, dtype=DTYPE)
+# model = model.half()
+model.tokenizer.padding_side = "left"
+
+#%%
+cfact_retain = datasets.load_from_disk(f"~/mechanistic-unlearning/experiments/scaling_counterfact/{MODEL_NAME.replace('/', '_')}_cfact_forget.hf")["test"]
+
+# Important columns: prompt, correct_token, edit_str, localization_str
+cfact_forget = pd.read_csv(f"~/mechanistic-unlearning/experiments/scaling_counterfact/cfact_forget_{MODEL_NAME.replace('/', '_')}.csv")[:DATASET_LENGTH]
+mcq_path = "/root/mechanistic-unlearning/tasks/facts/data/counterfact_mc_questions.parquet"
+mcq = pd.read_parquet(mcq_path)
+mcq['case_id'] = mcq['prompt_id']
+merged_q = pd.merge(mcq, cfact_forget, how="inner", on="case_id")
+
+def generate_binary_string(length):
+    result = ['0'] * length
+
+    # Ensure at least one '1' in a random position
+    guaranteed_one_pos = random.randint(0, length - 1)
+    result[guaranteed_one_pos] = '1'
+
+    # Fill in the rest with 10% chance of being '1'
+    for i in range(length):
+        if i != guaranteed_one_pos:
+            result[i] = '1' if random.random() < 0.1 else '0'
+
+    return ''.join(result)
+
+# cfact_forget["prompt"] = "Fact: The Eiffel Tower is in Paris. Fact: " + cfact_forget["prompt"]
+mlps_len = model.cfg.n_layers
+if MODE == "MANUAL":
+    cfact_forget["localization_str"] = '1111111111100000000000000000'
+    # cfact_forget["localization_str"] = '1111111111111000000000000000'
+    # pass
+if MODE == "RANDOM":
+    mlps_len = len(cfact_forget["localization_str"].iloc[0])
+    cfact_forget["localization_str"] = [generate_binary_string(mlps_len) for _ in range(len(cfact_forget))]
+elif MODE == "ALL":
+    mlps_len = len(cfact_forget["localization_str"].iloc[0])
+    cfact_forget["localization_str"] = ["1" * mlps_len for _ in range(len(cfact_forget))]
+elif MODE == "ATTN":
+    cfact_forget["localization_str"] = ["0" * mlps_len for _ in range(len(cfact_forget))]
+
+pile_retain = load_dataset("EleutherAI/the_pile_deduplicated", split="train", streaming=True)
+
+#%%
+import wandb
+from itertools import cycle
+import random
+from functools import partial
+
+wandb.login()
+
+def collate_fn(batch, tokenizer):
+    def generate_mcqs(questions, correct_answers, incorrect_answers, edit_answers):
+        formatted_questions = []
+        orig_labels = []
+        edit_labels = []
+        icl_prompt = "Where is the Eiffel Tower?\nA. Seattle\nB. Rome\nC. Paris\nD. Madrid\nAnswer: C\n"
+
+        for q, correct, incorrect, edit in zip(questions, correct_answers, incorrect_answers, edit_answers):
+            # Combine correct and incorrect answers
+            options = list(incorrect) + [correct]
+            random.shuffle(options)
+
+            # Map options to labels
+            option_labels = ['A', 'B', 'C', 'D']
+
+            # Find correct answer's label
+            correct_idx = options.index(correct)
+            correct_label = option_labels[correct_idx]
+            if edit not in options:
+                options[(correct_idx+1)%4] = edit
+
+            edit_label = option_labels[options.index(edit)]
+
+            labeled_options = [f"{label}. {opt}" for label, opt in zip(option_labels, options)]
+
+            # Format question string
+            question_text = icl_prompt + f"{q}\n" + "\n".join(labeled_options) + "\nAnswer:"
+            # question_text = f"{q}\n" + "\n".join(labeled_options) + "\nAnswer:"
+            formatted_questions.append(question_text)
+
+            orig_labels.append(" " + correct_label)
+            edit_labels.append(" " + edit_label)
+
+
+        return formatted_questions, orig_labels, edit_labels
+
+    qs = [q['question'] for q in batch]
+    corrects = [q['target_true'] for q in batch]
+    incorrects = [q['targets_false'] for q in batch]
+    edits = [q['edit_string'].strip() for q in batch]
+    questions, orig_labels, edit_labels = generate_mcqs(qs, corrects, incorrects, edits)
+
+    tokenizer.padding_side = "right"
+    orig_labels_toks = tokenizer(orig_labels, return_tensors="pt", padding=True, add_special_tokens=False)['input_ids'][:, 0]
+    edit_labels_toks = tokenizer(edit_labels, return_tensors="pt", padding=True, add_special_tokens=False)['input_ids'][:, 0]
+    tokenizer.padding_side = "left"
+    question_toks = tokenizer(questions, return_tensors="pt", padding=True)['input_ids']
+
+    return (question_toks, orig_labels_toks, edit_labels_toks)
+
+def unfreeze_mlps(model, mlp_str):
+    # Unfreeze MLPs according to mlp_str
+    for block, do_train in zip(model.blocks, mlp_str):
+        if do_train == "1":
+            block.mlp.W_out.requires_grad = True
+
+
+lambda_inject = 2e-4
+lambda_retain = 1e-3
+lambda_sft = 1e-3
+learning_rate = 1e-5
+BATCH_SIZE = 24
+epochs=20
+
+ce_loss = torch.nn.CrossEntropyLoss(ignore_index = model.tokenizer.pad_token_id)
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer=optimizer, T_max=epochs)
+
+run = wandb.init(
+    project="mech-unlearning",  # Specify your project
+    name=f"cfact_scale_{MODEL_NAME.replace('/', '_')}_{DATASET_LENGTH}_{MODE}",
+    config={                        # Track hyperparameters and metadata
+        "dataset_length": DATASET_LENGTH,
+        "learning_rate": learning_rate,
+        "epochs": epochs,
+        "lambda_inject": lambda_inject,
+        "lambda_retain": lambda_retain,
+        "lambda_sft": lambda_sft,
+        "batch_size": BATCH_SIZE,
+        "model": MODEL_NAME,
+        "mode": MODE
+    },
+)
+# Dataloaders
+cfact_retain_iter = iter(cycle(DataLoader(cfact_retain.with_format("torch"), batch_size=BATCH_SIZE)))
+pile_retain_iter = iter(DataLoader(pile_retain.with_format("torch"), batch_size=BATCH_SIZE))
+#%%
+for epoch in range(epochs):
+    print(f"EPOCH: {epoch}")
+    torch.cuda.empty_cache()
+    gc.collect()
+    
+    for localization_str in cfact_forget['localization_str'].unique():
+        torch.cuda.empty_cache()
+        gc.collect()
+        if MODE == "ATTN":
+            for name, param in model.named_parameters():
+                if "attn" in name:
+                    param.requires_grad = True
+                else:
+                    param.requires_grad = False 
+        else:
+            for param in model.parameters():
+                param.requires_grad = False
+            unfreeze_mlps(model, localization_str)
+        # if MODE == "ATTN":
+        #     for attn in 
+
+        forget_prompts = cfact_forget[cfact_forget['localization_str'] == localization_str]['prompt'].tolist()
+        forget_labels = cfact_forget[cfact_forget['localization_str'] == localization_str]['edit_string'].tolist()
+        forget_orig_labels = torch.tensor(cfact_forget[cfact_forget['localization_str'] == localization_str]['correct_token'].tolist(), device=device)
+
+        forget_toks = model.tokenizer(forget_prompts, return_tensors="pt", padding=True)["input_ids"].to(device)
+
+        model.tokenizer.padding_side = "right"
+        forget_labels = model.tokenizer(forget_labels, return_tensors="pt", padding=True, add_special_tokens=False)["input_ids"][:, 0].to(device)
+        model.tokenizer.padding_side = "left"
+
+        optimizer.zero_grad()
+        # Process in batches of BATCH_SIZE
+        
+        pbar = tqdm(range(0, len(forget_toks), BATCH_SIZE))
+        for i in pbar:
+            torch.cuda.empty_cache()
+            gc.collect()
+            # Injection
+            with torch.cuda.amp.autocast(dtype=DTYPE):
+                forget_logits = model(forget_toks[i:i+BATCH_SIZE])[:, -1, :]
+            forget_preds = torch.argmax(forget_logits, dim=-1).detach()
+            # for pred, orig, edit in zip(forget_preds, forget_orig_labels[i:i+BATCH_SIZE], forget_labels[i:i+BATCH_SIZE]):
+            #     print(model.tokenizer.decode(pred), model.tokenizer.decode(orig), model.tokenizer.decode(edit))
+
+            # Percent of time model predicts original label over edit label
+            # forget_acc = torch.sum(forget_preds == forget_orig_labels[i:i+BATCH_SIZE]).item() / forget_preds.shape[0]
+            forget_acc = torch.sum(forget_logits[range(forget_logits.size(0)), forget_labels[i:i+BATCH_SIZE]] < forget_logits[range(forget_logits.size(0)), forget_orig_labels[i:i+BATCH_SIZE]]).item() / forget_preds.shape[0]
+
+            # Percent of time model predicts edited label over original label
+            # edit_acc = torch.sum(forget_preds == forget_labels[i:i+BATCH_SIZE]).item() / forget_preds.shape[0]
+            edit_acc = torch.sum(forget_logits[range(forget_logits.size(0)), forget_labels[i:i+BATCH_SIZE]] > forget_logits[range(forget_logits.size(0)), forget_orig_labels[i:i+BATCH_SIZE]]).item() / forget_preds.shape[0]
+
+            loss = ce_loss(forget_logits, forget_labels[i:i+BATCH_SIZE])
+            inject_loss = loss.item()
+            loss.backward()
+
+            torch.cuda.empty_cache()
+            gc.collect()
+            # Retain
+            cfact_retain_batch = next(cfact_retain_iter)
+            retain_toks = model.tokenizer(cfact_retain_batch['prompt'], return_tensors="pt", padding=True)["input_ids"].to(device)
+
+            model.tokenizer.padding_side = "right"
+            retain_labels = model.tokenizer(cfact_retain_batch['true_string'], return_tensors="pt", padding=True, add_special_tokens=False)["input_ids"][:, 0].to(device)
+            model.tokenizer.padding_side = "left"
+            with torch.cuda.amp.autocast(dtype=DTYPE):
+                loss = ce_loss(model(retain_toks)[:, -1, :], retain_labels)
+            retain_loss = loss.item()
+            loss.backward()
+
+            torch.cuda.empty_cache()
+            gc.collect()
+
+            # SFT
+            pile_retain_batch = next(pile_retain_iter)
+            pile_toks = model.tokenizer(pile_retain_batch['text'], return_tensors="pt", padding=True, truncation=True, max_length=100)['input_ids'].to(device)
+            pile_labels = pile_toks[:, 1:]
+            with torch.cuda.amp.autocast(dtype=DTYPE):
+                logits = model(pile_toks)[:, :-1, :]
+            loss = ce_loss(logits.reshape(-1, logits.size(-1)), pile_labels.reshape(-1))
+            sft_loss = loss.item()
+            loss.backward()
+
+            optimizer.step()
+            scheduler.step()
+
+            if i % 5 == 0:
+                with torch.no_grad():
+                    # mcq eval
+                    mcq_iter = iter(DataLoader(
+                        Dataset.from_pandas(merged_q), 
+                        collate_fn=partial(collate_fn, tokenizer=model.tokenizer), 
+                        batch_size=32
+                    ))
+                    mc_edit_acc = 0
+                    mc_orig_acc = 0
+                    mc_edit_over_orig_acc = 0
+                    total = 0
+                    for mcq_batch in mcq_iter:
+                        question_toks, orig_labels, edit_labels = mcq_batch
+                        question_toks = question_toks.to(device)
+                        orig_labels = orig_labels.to(device)
+                        edit_labels = edit_labels.to(device)
+
+                        with torch.cuda.amp.autocast(dtype=DTYPE):
+                            logits = model(question_toks)[:, -1, :]
+                        preds = torch.argmax(logits, dim=-1)
+                        mc_edit_acc += (preds == edit_labels).float().sum().item() 
+                        mc_orig_acc += (preds == orig_labels).float().sum().item()
+                        mc_edit_over_orig_acc += (logits[range(len(edit_labels)), edit_labels] > logits[range(len(orig_labels)), orig_labels]).float().sum().item()
+                        total += len(edit_labels)
+                    mc_edit_acc /= total
+                    mc_orig_acc /= total
+                    mc_edit_over_orig_acc /= total
+                    wandb.log({
+                        "mc_edit_over_orig_acc": mc_edit_over_orig_acc, 
+                        "mc_edit_acc": mc_edit_acc, 
+                        "mc_orig_acc": mc_orig_acc
+                    })
+                    # if mc_edit_acc > 0.9: break
+
+            pbar.set_postfix(
+                inject_loss=inject_loss, 
+                retain_loss=retain_loss, 
+                sft_loss=sft_loss, 
+                forget_acc=forget_acc, 
+                edit_acc=edit_acc,
+                mc_edit_acc=mc_edit_acc,
+                mc_orig_acc=mc_orig_acc,
+                mc_edit_over_orig_acc=mc_edit_over_orig_acc
+            )
+            wandb.log({
+                "inject_loss": inject_loss, 
+                "retain_loss": retain_loss, 
+                "sft_loss": sft_loss, 
+                "forget_acc": forget_acc, 
+                "edit_acc": edit_acc, 
+            })
+            torch.cuda.empty_cache()
+            gc.collect()
+#%%
+with torch.no_grad():
+    forget_acc = 0
+    edit_acc = 0
+    total = 0
+    for localization_str in cfact_forget['localization_str'].unique():
+        pbar = tqdm(range(0, len(forget_toks), BATCH_SIZE))
+        for i in pbar:
+            torch.cuda.empty_cache()
+            gc.collect()
+            with torch.cuda.amp.autocast(dtype=DTYPE):
+                forget_logits = model(forget_toks[i:i+BATCH_SIZE])[:, -1, :]
+            forget_preds = torch.argmax(forget_logits, dim=-1).detach()
+            # for pred, orig, edit in zip(forget_preds, forget_orig_labels[i:i+BATCH_SIZE], forget_labels[i:i+BATCH_SIZE]):
+            #     print(model.tokenizer.decode(pred), model.tokenizer.decode(orig), model.tokenizer.decode(edit))
+
+            # Percent of time model predicts original label over edit label
+            # forget_acc = torch.sum(forget_preds == forget_orig_labels[i:i+BATCH_SIZE]).item() / forget_preds.shape[0]
+            forget_acc += torch.sum(forget_logits[range(forget_logits.size(0)), forget_labels[i:i+BATCH_SIZE]] < forget_logits[range(forget_logits.size(0)), forget_orig_labels[i:i+BATCH_SIZE]]).item()
+
+            # Percent of time model predicts edited label over original label
+            # edit_acc = torch.sum(forget_preds == forget_labels[i:i+BATCH_SIZE]).item() / forget_preds.shape[0]
+            edit_acc += torch.sum(forget_logits[range(forget_logits.size(0)), forget_labels[i:i+BATCH_SIZE]] > forget_logits[range(forget_logits.size(0)), forget_orig_labels[i:i+BATCH_SIZE]]).item()
+            total += forget_preds.shape[0]
+            pbar.set_postfix(forget_acc=forget_acc/total, edit_acc=edit_acc/total)
+    forget_acc /= total
+    edit_acc /= total
+    wandb.log({"final_forget_acc": forget_acc, "final_edit_acc": edit_acc})
+with torch.no_grad():
+    # mcq eval
+    mcq_iter = iter(DataLoader(
+        Dataset.from_pandas(merged_q), 
+        collate_fn=partial(collate_fn, tokenizer=model.tokenizer), 
+        batch_size=32
+    ))
+    mc_edit_acc = 0
+    mc_orig_acc = 0
+    mc_edit_over_orig_acc = 0
+    total = 0
+    for mcq_batch in mcq_iter:
+        question_toks, orig_labels, edit_labels = mcq_batch
+        question_toks = question_toks.to(device)
+        orig_labels = orig_labels.to(device)
+        edit_labels = edit_labels.to(device)
+
+        with torch.cuda.amp.autocast(dtype=DTYPE):
+            logits = model(question_toks)[:, -1, :]
+        preds = torch.argmax(logits, dim=-1)
+        mc_edit_acc += (preds == edit_labels).float().sum().item() 
+        mc_orig_acc += (preds == orig_labels).float().sum().item()
+        mc_edit_over_orig_acc += (logits[range(len(edit_labels)), edit_labels] > logits[range(len(orig_labels)), orig_labels]).float().sum().item()
+        total += len(edit_labels)
+    mc_edit_acc /= total
+    mc_orig_acc /= total
+    mc_edit_over_orig_acc /= total
+    wandb.log({
+        "final_mc_edit_over_orig_acc": mc_edit_over_orig_acc, 
+        "final_mc_edit_acc": mc_edit_acc, 
+        "final_mc_orig_acc": mc_orig_acc
+    })
+# %%
+torch.save(model.state_dict(), f"/root/mechanistic-unlearning/experiments/scaling_counterfact/{MODEL_NAME.replace('/', '_')}_forget_{DATASET_LENGTH}_{MODE}.pth")
+print('saved')
+wandb.finish()
+# %%
+# model.load_state_dict(torch.load(f"/root/mechanistic-unlearning/experiments/scaling_counterfact/{MODEL_NAME.replace('/', '_')}_forget_{DATASET_LENGTH}facts.pth"))
+# %%
